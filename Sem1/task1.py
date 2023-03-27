9fd8b1e7c32157d9638ec72fb788e23cb4ac973f
Mariya Khlyustova <7047373@mail.ru>
2023-03-28 01:16:27 +0300

    git init

---
+++ Sem1/task1.py
@@ -0,0 +1,33 @@
+# a = int(input('введите первое число: '))
+# b = int(input('введите второе число: '))
+# if a == b*b or b == a*a:
+#     print('да')
+# else: print('нет')
+
+# a = int(input('введите первое число: '))
+# b = int(input('введите второе число: '))
+# c = int(input('введите третье число: '))
+# d = int(input('введите четвертое число: '))
+# e = int(input('введите пятое число: '))
+# max = a
+# if b > max: max = b
+# if c > max: max = c
+# if d > max: max = d
+# if e > max: max = e
+# print(max)
+
+# i = int(input('порядковый номер вагона: '))
+# j = int(input('номер вагона: '))
+# if i == j:
+#     print('невозможно определить кол-во вагонов')
+# else:
+#     print(f" в поезде {i + j - 1} вагонов")
+
+# desk = 0
+# for i in range(3):
+#     a = int(input())
+#     if a % 2 == 0:
+#         desk += (a // 2)
+#     else:
+#         desk += (a // 2) + 1
+# print(desk)
