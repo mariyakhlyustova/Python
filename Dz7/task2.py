d59c68ed0f764eab7d902dc219c0eb8f3719ebba
Mariya Khlyustova <7047373@mail.ru>
2023-06-09 17:23:25 +0300

    ddfghjk

--- Dz7/task2.py
+++ Dz7/task2.py
@@ -12,7 +12,7 @@
 # 3 6 9 12 15 18
 # 4 8 12 16 20 24
 
-def print_operation_table(operation, num_rows = 4, num_columns = 6):
+def print_operation_table(operation, num_rows = 6, num_columns = 6):
     for i in range(1, num_rows + 1):
         for j in range(1, num_columns + 1):
             if i == 1:
@@ -23,4 +23,4 @@ def print_operation_table(operation, num_rows = 4, num_columns = 6):
                 print(operation(i, j), end=' ')
         print()
 
-print_operation_table(lambda x, y: x ** y)
+print_operation_table(lambda x, y: x + y)
